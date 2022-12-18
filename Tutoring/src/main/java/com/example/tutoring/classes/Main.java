d80df3839811e89e130d96f679833cd9b699ab78
Damian1457 <damianwasik.coach@gmail.com>
2022-12-18 09:37:15 +0100

    Scissor Papper Rock

---
+++ Tutoring/src/main/java/com/example/tutoring/classes/Main.java
@@ -0,0 +1,18 @@
+package com.example.tutoring.classes;
+
+import com.example.tutoring.classes.view.UserInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterface gui = new UserInterface();
+    }
+}
+
+//1. Chciałbym zrobić oddzielną klasę z któej będę pobierał samoloty, nie musząc ich tworzyć w konstruktorze.
+//2. Umieścić samoloty w bazie danych.
+//3. Automatyczne pobieranie listy z bazy danych.
+//4. Po zalogowaniu będę widział listę samolotów, i będę mógł to dodawać w przeglądarce.
+
+
+
+
